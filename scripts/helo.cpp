5b51707e2414a7e23e304d7d0417688a1ca5be99
SachinyadavAug20 <samtagon777@gmail.com>
2026-01-11 18:14:53 +0530

    Deleted un used class

--- scripts/helo.cpp
+++ scripts/helo.cpp
@@ -31,23 +31,5 @@ int main() {
   vector<int> num = {100, 200, 300, 400};
   int k = 2;
   cout << maxSubarraySum(num, k);
+  printf("Hello world\n");
 }
-
-class Solution {
-public:
-  int maxSubarraySum(vector<int> &arr, int k) {
-    // prefix sum 1 index
-    vector<int> prefixsum(arr.size() + 1);
-    for (int i = 1; i <= arr.size(); i++) {
-      prefixsum[i] = prefixsum[i - 1] + arr[i - 1];
-    }
-    printvector(prefixsum);
-    int maxnum = 0;
-    for (int i = 0; i < arr.size() && i + k <= arr.size(); i++) {
-      if (maxnum < (prefixsum[i + k] - prefixsum[i])) {
-        maxnum = (prefixsum[i + k] - prefixsum[i]);
-      }
-    }
-    return maxnum;
-  }
-};
